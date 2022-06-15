7cc01dfb391af17f18602101bc297db1d8eda90e
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-15 19:28:52 +0100

    🚀 config test

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+import pytest
+from .router import create_app
+
+
+@pytest.fixture()
+def App():
+    """Configures the app for testing
+
+    Sets app config variable ``TESTING`` to ``True``
+
+    :return: App for testing
+    """
+
+    #app.config['TESTING'] = True
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+@pytest.fixture()
+def Client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def Runner(app):
+    return app.test_cli_runner()
