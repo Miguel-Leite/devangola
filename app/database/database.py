1b437371e812c6e52e8dc24bf2c6c999ac159457
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-15 19:28:34 +0100

    🚀 config path in database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,11 @@
 import json
+import os
 
-def getDatabase() -> object :
-    with open("database.json",encoding='utf-8') as database_json:
+cwd = os. getcwd()
+database = f'{cwd}/app/database/database.json'
+
+def getDatabase() -> dict :
+    with open(database,encoding='utf-8') as database_json:
         data = json.load(database_json)
 
     return data
