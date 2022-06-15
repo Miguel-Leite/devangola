bcebe30f5908a16c915a85251e2aaeff01f804af
Miguel Leite <miguelleite200leite@gmail.com>
2022-06-15 19:27:57 +0100

    🚀 Write unit test

---
+++ tests/test_provinces.py
@@ -0,0 +1,11 @@
+from multiprocessing.connection import Client
+import unittest
+
+
+class TestProvinces(unittest.TestCase):
+
+    def test_provinces(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
